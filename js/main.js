0d872fa5102b24e4d663b3d020e14f8e01d6c771
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2021-05-30 09:47:44 +0700

    подключение файла main.js с двумя функциями по нахождению случайных чисел getRandomNumber() и getCoordinates()

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно. 
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  if (max <= min) {
+    console.log('max должно быть больше min на пять единиц');
+
+  }
+
+  //   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум включается, минимум включается
+
+
+}
+// console.log(getRandomNumber(3, 10));
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+// https://myrusakov.ru/js-random-numbers.html
+
+function getCoordinates(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  if (max <= min) {
+    console.log('max должно быть больше min на пять единиц');
+  }
+
+  return Math.random() * (max - min) + min;
+
+}
+// console.log(getCoordinates(1, 3));
