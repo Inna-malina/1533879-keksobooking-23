534ec6bf689c24c76418789eaa737591a99ec937
Inna Cherkashina <inna.cherkashina.1979@mail.ru>
2021-05-30 20:45:24 +0700

    внесла правки в js/main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,15 @@
-// Функция, возвращающая случайное целое число из переданного диапазона включительно. 
+// Функция, возвращающая случайное целое число из переданного диапазона включительно.
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 function getRandomNumber(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (max <= min) {
-    console.log('max должно быть больше min на пять единиц');
-
+    return false;
   }
-
   //   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум включается, минимум включается
-
-
 }
-// console.log(getRandomNumber(3, 10));
+getRandomNumber(3, 10);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 // https://myrusakov.ru/js-random-numbers.html
@@ -23,22 +19,20 @@ function getCoordinates(min, max) {
   max = Math.floor(max);
 
   if (max <= min) {
-    console.log('max должно быть больше min на пять единиц');
+    return false;
   }
-
   return Math.random() * (max - min) + min;
-
 }
-// console.log(getCoordinates(1, 3));
+getCoordinates(1, 3);
 
 
 // Функция для проверки максимальной длины строки
 function getStrLength(str, max) {
   if (str.length >= max) {
-    console.log(true);
+    return true;
   }
   if (str.length < max) {
-    console.log(false);
+    return false;
   }
 }
 getStrLength('werqwersdfghjkl', 10);
